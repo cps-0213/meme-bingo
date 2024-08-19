c8f48bfa073616849e4b7500cddd4e8cec223b20
cps <newmuneo0083@hotmail.co.jp>
2024-08-19 19:13:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const BINGO_SIZE = 5;
-const MAX_NUMBER = 24;  // 画像の数に合わせて24に変更 (freeを除く)
-let drawnNumbers = [];
+const socket = io('http://localhost:3000');
 
-// 固定のビンゴカード画像配列
+let drawnNumbers = {};  // 抽選結果を管理するオブジェクト
+
 const fixedImages = [
     ["a1.png", "b1.png", "c1.png", "d3.png", "e7.png"],
     ["a6.png", "b3.png", "c3.png", "d2.png", "e8.png"],
@@ -25,8 +25,40 @@ function generateBingoCard() {
             if (fixedImages[i][j] === "free.png") {
                 cell.classList.add('free');
             }
-            
+
             bingoCard.appendChild(cell);
         }
     }
 }
+
+function updateBingoCard() {
+    const bingoCard = document.getElementById('bingo-card');
+    const cells = bingoCard.getElementsByTagName('div');
+
+    for (let i = 0; i < cells.length; i++) {
+        const img = cells[i].getElementsByTagName('img')[0];
+        const imgName = img.src.split('/').pop().split('.').shift();  // ファイル名から番号部分を抽出
+
+        if (drawnNumbers[imgName]) {
+            cells[i].style.backgroundColor = '#ccc';  // グレーアウト
+        }
+    }
+}
+
+// サーバーから新しい番号が送信されてきた時の処理
+socket.on('newNumber', (number) => {
+    drawnNumbers[number] = true;
+    updateBingoCard();
+});
+
+// サーバーから現在の抽選結果を受け取る
+socket.emit('getDrawnNumbers');
+socket.on('drawnNumbers', (numbers) => {
+    drawnNumbers = numbers;
+    updateBingoCard();
+});
+
+// ページロード時にビンゴカードを生成
+window.onload = function() {
+    generateBingoCard();
+};
